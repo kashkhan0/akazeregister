15c2d651b7b1640d52f489e6d2cd1086cbd1a6f8
kashkhan0 <kashkhan@gmail.com>
2018-01-04 19:28:00 -0800

    Update akazeregister4.cpp

--- akazeregister4.cpp
+++ akazeregister4.cpp
@@ -1,4 +1,6 @@
 // g++ -w -std=c++14 akazeregister4.cpp -o akazeregister4 `pkg-config opencv --cflags --libs`
+// CPU based AKAZE features used to overlay image1 over image2.
+// NUmber of features can be increased by reducing the AKAZE threshold.
 
 #include <algorithm>
 #include <cstdlib>
@@ -23,6 +25,7 @@ bool isfile(const std::string &name) {
 }
 
 vector<Point2f> Points(vector<KeyPoint> keypoints) {
+  // Convert OpenCV Keyoints to vector of OpenCV Point2F for findhomography
   vector<Point2f> res;
   for (unsigned i = 0; i < keypoints.size(); i++) {
     res.push_back(keypoints[i].pt);
@@ -32,20 +35,26 @@ vector<Point2f> Points(vector<KeyPoint> keypoints) {
 
 void pastemasklocal(InputOutputArray backU, InputArray foreU,
                     InputArray maskU) {
+  //paste foreU over backU based on maskU
   Mat dst, emask, gmask, foreground, background, mask, output;
-  int gs = 3;
-
+  int gs = 3; // kernel radius
+  
+  // Kernel for Erosion and Gaussian
   Mat element = getStructuringElement(MORPH_RECT, Size(2 * gs + 1, 2 * gs + 1),
                                       Point(gs, gs));
 
-  /// Apply the erosion operation
+  // Apply the erosion on mask to avoid black
   erode(maskU, emask, element);
-
+ 
+  // Apply Gaussian on Mask to smooth transition
   GaussianBlur(emask, gmask, Size(7, 7), 0, 0);
 
+  // make deep copies
   backU.copyTo(background);
   foreU.copyTo(foreground);
   gmask.copyTo(mask);
+  
+  // iterate through rows and columns
   for (int y = 0; y < background.rows; ++y) {
     int fY = y;
     if (fY < 0)
@@ -59,6 +68,8 @@ void pastemasklocal(InputOutputArray backU, InputArray foreU,
       if (fX < 0)
         continue;
       double opacity = 1;
+      
+      // get rgb values
       double r =
           ((double)foreground
                .data[fY * foreground.step + fX * foreground.channels() + 2]) /
@@ -71,11 +82,14 @@ void pastemasklocal(InputOutputArray backU, InputArray foreU,
           ((double)foreground
                .data[fY * foreground.step + fX * foreground.channels() + 0]) /
           255.;
+      // iterate through channels
       for (int c = 0; (r + g + b) > 0 && c < background.channels(); ++c) {
         unsigned char foregroundPx =
             foreground
                 .data[fY * foreground.step + fX * foreground.channels() + c];
-        double opacity = ((double)mask.data[fY * mask.step + fX]) / 255.;
+  
+        opacity = ((double)mask.data[fY * mask.step + fX]) / 255.;
+        //combine and write
         unsigned char outPx =
             background
                 .data[y * background.step + x * background.channels() + c];
@@ -89,7 +103,7 @@ void pastemasklocal(InputOutputArray backU, InputArray foreU,
 
 void akazefeat(Mat img1, vector<KeyPoint> &kp1, OutputArray desc1,
                float thresh = 0.0005f) {
-
+  // AKAZE feature extract
   Ptr<AKAZE> akaze = AKAZE::create();
   akaze->setThreshold(thresh);
   Ptr<Feature2D> detector;
@@ -100,6 +114,7 @@ void akazefeat(Mat img1, vector<KeyPoint> &kp1, OutputArray desc1,
 
 void match(InputArray desc1, InputArray desc2,
            vector<vector<DMatch>> &matches) {
+  // convenience wrapper
   Ptr<DescriptorMatcher> matcher =
       DescriptorMatcher::create("BruteForce-Hamming");
   matcher->knnMatch(desc2, desc1, matches, 2);
@@ -107,6 +122,7 @@ void match(InputArray desc1, InputArray desc2,
 
 void homogfind(vector<KeyPoint> kp1, vector<KeyPoint> kp2,
                vector<vector<DMatch>> matches, Mat &h, Mat &inlier_mask) {
+  // Find homography for matches using David Lowes Criterion
   vector<KeyPoint> matched1, matched2;
   vector<DMatch> matchesd;
   const double ransac_thresh = 4.0f;
@@ -129,6 +145,7 @@ int inliersum(Mat inliermask) {
 }
 
 void akaze2(Mat img1, Mat img2, Mat &H, int &nmatches, int &ninliers) {
+  // Find homography between img1 and img2
   vector<KeyPoint> kp1, kp2;
   Mat desc1, desc2, inlier_mask, h;
   vector<vector<DMatch>> matches, matchesd;
@@ -153,7 +170,7 @@ bool cmdOptionExists(char **begin, char **end, const std::string &option) {
 }
 
 void xformkp(vector<KeyPoint> k2a, vector<KeyPoint> &k2out, Mat H) {
-
+  // transform keypoints by Homography H
   vector<Point2f> points2;
   vector<KeyPoint> k2;
   perspectiveTransform(Points(k2a), points2, H);
@@ -169,6 +186,7 @@ void xformkp(vector<KeyPoint> k2a, vector<KeyPoint> &k2out, Mat H) {
 
 void pastewarp(InputOutputArray montf, InputArray img, vector<Point2f> src,
                vector<Point2f> dst) {
+  // warp and paste
   int w, h, wi, hi, success;
   Mat montfc, imgc, warpedimg, warpedmask;
   montf.copyTo(montfc);
@@ -192,42 +210,58 @@ void pastewarp(InputOutputArray montf, InputArray img, vector<Point2f> src,
 }
 
 void imsave(const string fname, InputArray img) {
+  // imwrite wrapper
   imwrite(fname, img);
   cout << "imsave " << fname << endl;
 }
 
 int main(int argc, char **argv) {
-
-  string fn1 = "smwf0295paste.jpg";
-  string fnscene = "smwf0295crop.jpg";
+  // ./akazeregister imgscene img1
+  string fn1 = "img1.jpg";
+  string fnscene = "imgscene.jpg";
 
   if (argc > 2) {
     fn1 = argv[2];
     fnscene = argv[1];
   }
+  else{
+    cout << "./akazeregister imgscene img1" << endl;
+    return -1;
+  }
 
   Mat img1 = imread(fn1, 129);
-  // Mat img1 = imread("smwf0295crop.jpg",129);
   Mat imgscene = imread(fnscene, 129);
+  
+  if (img1.rows < 10 || imgscene.rows < 10) {
+    cout << "Bad imageset imgscene: " <<  imgscene.size() << " img1: " <<  img1.size() << endl;
+    return -1;
+  }
+  
   Mat H;
   int nm, ni;
+  // find Homography
   akaze2(imgscene, img1, H, nm, ni);
   cout << H << endl << ni << " inliers" << endl;
   int w = imgscene.cols;
   int h = imgscene.rows;
   int wi = img1.cols;
   int hi = img1.rows;
-
+ 
+  // create white mask
   Mat imgwhite(hi, wi, CV_8UC1, Scalar(255, 255, 255));
   Mat warpedimg, warpedmask, gray;
   cvtColor(imgscene, gray, CV_BGR2GRAY);
   cvtColor(gray, imgscene, CV_GRAY2BGR);
 
+  //warp img1
   warpPerspective(img1, warpedimg, H, cv::Size(w, h), INTER_CUBIC,
                   BORDER_CONSTANT, 0);
+  // warp mask
   warpPerspective(imgwhite, warpedmask, H, cv::Size(w, h), INTER_CUBIC,
                   BORDER_CONSTANT, 0);
+  // overlay onto imgscene
   pastemasklocal(imgscene, warpedimg, warpedmask);
+  //save 
   imsave("awarped.png", warpedimg);
   imsave("ascene.jpg", imgscene);
 }
